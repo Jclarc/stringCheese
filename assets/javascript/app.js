e831df8eb5e690b13e037193772a5d46a42a8cbe
Jacob Clark <jtclark@knights.ucf.edu>
2016-08-16 20:55:10 -0400

    changes tables print out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -69,8 +69,17 @@ $(document).ready(function () {
                     e.preventDefault();
 
                     userInput = JSON.stringify($('#userInput').val());
-                    console.log("'"+userInput+"'");
+                    console.log(""+userInput+"");
 
+
+
+
+
+
+
+
+
+                    console.log(response);
                     $('#r1').append(userInput);
                     $('#r1').append("<br>");
                     $('#r1').append("Anger Score: " + anger + "%");
@@ -82,10 +91,7 @@ $(document).ready(function () {
                     $('#r1').append("Joy Score: " + joy + "%");
                     $('#r1').append("<br>");
                     $('#r1').append("Sadness Score: " + sadness + "%");
-                    $('#r1').append("<br>");
 
-                    
-                    console.log(response);
                     console.log("Anger Score: " + anger + "%");
                     console.log("Disgust Score: " + disgust + "%");
                     console.log("Fear Score: " + fear + "%");
