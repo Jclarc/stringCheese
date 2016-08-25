66649dcc5808f58e66a0ab0afd3b8f568cb2f74a
Keely <keelylumbra@knights.ucf.edu>
2016-08-25 18:57:04 -0400

    fixing push issues

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,27 @@
+
+var config = {
+    apiKey: "AIzaSyA4h2hleSZ5_-ydKxZ-w-_G22mr4JaE3Is",
+    authDomain: "string-cheese.firebaseapp.com",
+    databaseURL: "https://string-cheese.firebaseio.com",
+    storageBucket: "string-cheese.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database()
+
 var audioElement = document.createElement('audio');
+$(".scores").hide(".scores");
+    $("#win").hide("#win");
+    $("#lose").hide("#lose");
+    $("#recentUsers").hide("#recentUsers");
 
 
 $(document).ready(function () {
 
-    $(".scores").hide(".scores");
+    
     var name = "";
     var score = 0
+
 
     window.onload = function () {
 //Rocky music on startup
@@ -22,11 +38,11 @@ $(document).ready(function () {
             $(".address-bar").hide('.address-bar');
             $(".player-form").hide('.player-form');
             $(".game-area").css("text-align",
-                "center", "width", "500px").html("<form class='arena'><textarea class='form-control'id='userInput' rows='3' maxlength='160' placeholder='Go for the gold Frodo Douchebaggins.'></textarea><input class='btn btn-default' type='submit' value='Submit' id='submit2'></form>");
+                "center", "width", "500px").html("<div class = 'italics'>Directions: You have 5 chances to get a collective score above 400 to win this thing. So type something hilariously terrible and hit submit.</div><form class='arena'><textarea class='form-control'id='userInput' rows='3' maxlength='160' placeholder='Go for the gold Frodo Douchebaggins.'></textarea><input class='btn btn-default' type='submit' value='Submit' id='submit2'></form>");
 
             var round=1;
             $(".scores").show(".scores");
-            $(".game-area").prepend("Give me your worst Dillhole");
+            $(".game-area").prepend("GIVE ME YOUR WORST, DILLHOLE.");
             $('#submit2').on('click', function (e) {
                 e.preventDefault();
                 var userInput = $('#userInput').val();
@@ -47,68 +63,82 @@ $(document).ready(function () {
 
                     var tones = {//object
                         "anger": response.document_tone.tone_categories[0].tones[0].score,
-                        // "disgust": response.document_tone.tone_categories[0].tones[1].score,
-                        // "fear": response.document_tone.tone_categories[0].tones[2].score,
-                        // "joy": response.document_tone.tone_categories[0].tones[3].score,
-                        // "sadness": response.document_tone.tone_categories[0].tones[4].score,
+                       
                     };
                     //sets up usable variables for each tone element already formated to percentages
                     var anger = Math.round(tones.anger * 100);
-                    // var disgust = Math.round(tones.disgust * 100);
-                    // var fear = Math.round(tones.fear * 100);
-                    // var joy = Math.round(tones.joy * 100);
-                    // var sadness = Math.round(tones.fear * 100);
+                  
 
                         if (round == 1) {
-
                             $('#r1').append(userInput);
                             $('#r1').append("<br>");
                             $('#r1').append("Anger Score: " + anger + "%");
-
+                            
                             score = anger;
                         } else if (round == 2) {
-                          
+                          //  $(".game-area").prepend("Your writing prompt is: " + prompts[round-1]);
                             $('#r2').append(userInput);
                             $('#r2').append("<br>");
                             $('#r2').append("Anger Score: " + anger + "%");
 
                             score = score + anger;
                         } else if (round == 3) {
-                          
+                           // $(".game-area").prepend("Your writing prompt is: " + prompts[round-1]);
                             $('#r3').append(userInput);
                             $('#r3').append("<br>");
                             $('#r3').append("Anger Score: " + anger + "%");
 
                             score = score + anger;
                         } else if (round == 4) {
-                           
+                           // $(".game-area").prepend("Your writing prompt is: " + prompts[round-1]);
                             $('#r4').append(userInput);
                             $('#r4').append("<br>");
                             $('#r4').append("Anger Score: " + anger + "%");
                             score = score + anger;
                         } else if (round == 5) {
-                          
+                          //  $(".game-area").prepend("Your writing prompt is: " + prompts[round-1]);
                             $('#r5').append(userInput);
                             $('#r5').append("<br>");
                             $('#r5').append("Anger Score: " + anger + "%");
 
                             score = score + anger;
+
+                            $(".game-area").hide(".game-area");
+                            $(".scores").empty();
+                            $(".scores").html("TOTAL SCORE: " + score);
+
+
+
+
+                        
+
+                            database.ref().push({
+
+                                name: name,
+                                score: score,
+                                date: firebase.database.ServerValue.TIMESTAMP
+
+
+                            });
+
+
+                          
+                                if (score > 400) {
+
+                                    $("#win").show("#win").append('<input class="btn btn-default reset" type="reset"  value="Reset"><br><Br></input><img src="./assets/images/winning.jpg"</img>')
+
+
+
+
+                                } else 
+                                    $("#lose").show("#lose").append('<input class="btn btn-default reset" type="reset"  value="Reset"><br><Br></input><img src="./assets/images/losing.png"</img>')
                         }
+
+                        $(".reset").on("click", function(){
+                            window.location.reload();
+})
                         round++;
                         drawChart();
-                    /*
-                    $('#r1').append(userInput);
-                    $('#r1').append("<br>");
-                    $('#r1').append("Anger Score: " + anger + "%");
-                    $('#r1').append("<br>");
-                    $('#r1').append("Disgust Score: " + disgust + "%");
-                    $('#r1').append("<br>");
-                    $('#r1').append("Fear Score: " + fear + "%");
-                    $('#r1').append("<br>");
-                    $('#r1').append("Joy Score: " + joy + "%");
-                    $('#r1').append("<br>");
-                    $('#r1').append("Sadness Score: " + sadness + "%");
-                    */
                 });
             })/*----------------------------------------------------------------------------------End Watson Ajax call---------------------*/
 
@@ -116,6 +146,10 @@ $(document).ready(function () {
             $(".game-area").html("Type your name, dipshit.");
         }
     });
+
+
+
+
     /*----------------------------------------------------------------END OF CLICK FUNCTION ---------------------------------------------------------------------*/
 // gauge code
     google.charts.load('current', {'packages': ['gauge']});
@@ -139,8 +173,34 @@ $(document).ready(function () {
         var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
 
         chart.draw(data, options);
+}
+
+});
 
 
-    }
-})
-/*---------------------------------END OF USERINPUT SUBMIT BUTTON ON CLICK FUNC------------------------*/
+  database.ref().orderByChild("score").on("child_added", function(childSnapshot, prevChildKey){
+
+
+
+                                // Store everything into a variable.
+                                var newName = childSnapshot.val().name;
+                                var newScore = childSnapshot.val().score;
+                                var newDate = childSnapshot.val().date;
+                                
+
+                                // Employee Info
+                              
+                                // Prettify the employee start
+                                var newDatePretty = moment(newDate).format("MM/DD/YY");
+                              // Calculate the months worked using hardcore math
+                                // To calculate the months worked
+                               
+
+                                // Calculate the total billed rate
+                               
+
+                                // Add each train's data into the table
+                                $("#highScores > tbody").append("<tr><td>" + newName + "</td><td>" + newScore + "</td><td>" + newDatePretty + "</td><tr>");
+                            });
+
+
